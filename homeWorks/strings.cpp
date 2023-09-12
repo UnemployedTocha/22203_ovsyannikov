14c36ee338d19cf798eb818e74c4372214dd1e00
Oleg Ovsyannikov <UnemployedTocha@mail.ru>
2023-09-13 00:09:58 +0700

    Added constructors and PrintString function

--- homeWorks/strings.cpp
+++ homeWorks/strings.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
-class MyString {
+#include <string>
+#include<cstring>
 
-    public:
+class Strings {
+public:
+    Strings(){
+        str = nullptr;
+        _length = 0;
+    }
+    Strings(char* incomingStr){
+        _length = strlen(incomingStr);
+        str = new char[_length + 1];
+        memcpy(str, incomingStr, _length+1);
+    }
+    Strings(Strings &incomingStr){
+        _length = incomingStr._length;
+        str = incomingStr.str;
+    }
+
+
+
+    void PrintString(){
+        std::cout << str << std::endl;
+    }
+
+    ~Strings(){
+
+    }
+private:
+    char* str;
+    int _length;
+
 };
 
 int main(){
