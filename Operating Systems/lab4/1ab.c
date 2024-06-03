bee967ec1f1349bf59dbd83712bf1ac505aec7e8
Oleg Ovsyannikov <143802667+UnemployedTocha@users.noreply.github.com>
2024-06-03 12:54:14 +0700

    Update 1ab.c

--- Operating Systems/lab4/1ab.c
+++ Operating Systems/lab4/1ab.c
@@ -5,18 +5,18 @@ int global_notinit;
 const int global_const = 11;
 
 void show_variables_addresses() {
-    int local_init = 20;
-    int local_notinit;
-    static int local_static = 21;
-    const int local_const = 22;
-
-    printf("global_init\t%d\t%p\n", global_init, &global_init);
-    printf("global_notinit\t%d\t%p\n", global_notinit, &global_notinit);
-    printf("global_const\t%d\t%p\n", global_const, &global_const);
-    printf("local_init\t%d\t%p\n", local_init, &local_init);
-    printf("local_notinit\t%d\t%p\n", local_notinit, &local_notinit);
-    printf("local_static\t%d\t%p\n", local_static, &local_static); 
-    printf("local_const\t%d\t%p\n", local_const, &local_const);
+	int local_init = 20;
+	int local_notinit;
+	static int local_static = 21;
+	const int local_const = 22;
+	
+	printf("global_init\t%d\t%p\n", global_init, &global_init);
+	printf("global_notinit\t%d\t%p\n", global_notinit, &global_notinit);
+	printf("global_const\t%d\t%p\n", global_const, &global_const);
+	printf("local_init\t%d\t%p\n", local_init, &local_init);
+	printf("local_notinit\t%d\t%p\n", local_notinit, &local_notinit);
+	printf("local_static\t%d\t%p\n", local_static, &local_static); 
+	printf("local_const\t%d\t%p\n", local_const, &local_const);
 }
 
 int show_maps() {
@@ -38,6 +38,6 @@ int show_maps() {
 
 
 int main(int argc, char** argv) {
-    show_maps();
-    show_variables_addresses();
+	show_maps();
+	show_variables_addresses();
 }
